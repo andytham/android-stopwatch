1a64efb280fa3552e60c913966610d298e23b081
andy <thamandyy@gmail.com>
2019-04-03 22:32:26 -0400

    Start method

--- src/main/java/com/example/stopwatch/MainActivity.java
+++ src/main/java/com/example/stopwatch/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.stopwatch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onStartButton(View view){
+        // get operation
+        // get current time
+        TextView timer = findViewById(R.id.timer);
+        String currentTimeString = timer.getText().toString();
+        long currentTime = Long.parseLong(currentTimeString);
+
+        long startTime =  System.nanoTime();
+        //begin counting
+        while(true){
+            currentTime = currentTime + (System.nanoTime() - startTime);
+            timer.setText(Long.toString(currentTime));
+            startTime =  System.nanoTime();
+        }
+
     }
 }

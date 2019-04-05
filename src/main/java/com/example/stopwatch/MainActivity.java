6f9262835e1f7fc61483ffede97105aff93917b7
andy <thamandyy@gmail.com>
2019-04-04 21:49:17 -0400

    Milliseconds count, using timer with schedule

--- src/main/java/com/example/stopwatch/MainActivity.java
+++ src/main/java/com/example/stopwatch/MainActivity.java
@@ -4,46 +4,33 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-import org.apache.commons.lang3.time.StopWatch;
-public class MainActivity extends AppCompatActivity {
+import java.util.Timer;
+import java.util.TimerTask;
 
+public class MainActivity extends AppCompatActivity {
+    private int currentTime = 0;
+    private TextView display;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        display = (TextView) findViewById(R.id.timeDisplay);
     }
 
     public void onStartButton(View view){
         // get operation
         // get current time
-
-
-        StopWatch stopWatch = new StopWatch();
-        stopWatch.start();
-        Thread t = new Thread(){
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask(){
             @Override
             public void run(){
-                Boolean paused = false;
-                try {
-                    while(!isInterrupted()){
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                TextView timer = findViewById(R.id.timer);
-                                String currentTimeString = timer.getText().toString();
-                                long currentTime = Long.parseLong(currentTimeString);
-                                long time = System.nanoTime();
-                                timer.setText(Long.toString(time));
-
-                            }
-                        });
-
+                runOnUiThread(new Runnable(){
+                    public void run(){
+                        currentTime += 1;
+                        display.setText(Integer.toString(currentTime));
                     }
-                } catch (Exception e){
-
-                }
+                });
             }
-        };
-        t.start();
+        }, 0, 1); // every millisecond
     }
 }

623d949db8dea0c3a0d77bf12f644a4307e8a9ad
andy <thamandyy@gmail.com>
2019-04-09 21:41:24 -0400

    Pausing works

--- src/main/java/com/example/stopwatch/MainActivity.java
+++ src/main/java/com/example/stopwatch/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     private String timeString;
     private TextView display;
     private TextView startButton;
+    private Timer timer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,57 +34,61 @@ public class MainActivity extends AppCompatActivity {
     public void onStartButton(View view){
         // get operation
         // get current time
-        Timer timer = new Timer();
-        String buttonText = startButton.getText().toString();
-        if (buttonText == "START") {
-            startButton.setText("PAUSE");
-            
-            timer.scheduleAtFixedRate(new TimerTask() {
-                @Override
-                public void run() {
-                    runOnUiThread(new Runnable() {
-                        public void run() {
-                            currentTime += 1;
-                            milliseconds = currentTime % 1000;
-                            seconds = (currentTime / 1000) % 60;
-                            minutes = currentTime / 1000 / 60;
-                            hours = currentTime / 1000 / 360;
+        TimerTask timerTask = new TimerTask(){
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        currentTime += 1;
+                        milliseconds = currentTime % 1000;
+                        seconds = (currentTime / 1000) % 60;
+                        minutes = currentTime / 1000 / 60;
+                        hours = currentTime / 1000 / 360;
 
-                            stringMilliseconds = Integer.toString(milliseconds);
-                            if (milliseconds < 100) {
-                                if (milliseconds < 10) {
-                                    stringMilliseconds = "0" + stringMilliseconds;
-                                }
+                        stringMilliseconds = Integer.toString(milliseconds);
+                        if (milliseconds < 100) {
+                            if (milliseconds < 10) {
                                 stringMilliseconds = "0" + stringMilliseconds;
                             }
-                            stringSeconds = Integer.toString(seconds);
-                            if (seconds < 10) {
-                                stringSeconds = "0" + Integer.toString(seconds);
-                            }
-                            stringMinutes = Integer.toString(minutes);
-                            if (minutes < 10) {
-                                stringMinutes = "0" + stringMinutes;
-                            }
-                            stringHours = Integer.toString(hours);
-                            if (hours < 10) {
-                                stringHours = "0" + stringHours;
-                            }
-
-
-                            timeString = stringHours + ":" + stringMinutes + ":" + stringSeconds + ":" + stringMilliseconds;
-                            display.setText(timeString);
+                            stringMilliseconds = "0" + stringMilliseconds;
                         }
-                    });
-                }
-            }, 0, 1); // every millisecond
+                        stringSeconds = Integer.toString(seconds);
+                        if (seconds < 10) {
+                            stringSeconds = "0" + Integer.toString(seconds);
+                        }
+                        stringMinutes = Integer.toString(minutes);
+                        if (minutes < 10) {
+                            stringMinutes = "0" + stringMinutes;
+                        }
+                        stringHours = Integer.toString(hours);
+                        if (hours < 10) {
+                            stringHours = "0" + stringHours;
+                        }
+
+
+                        timeString = stringHours + ":" + stringMinutes + ":" + stringSeconds + ":" + stringMilliseconds;
+                        display.setText(timeString);
+                    }
+                });
+            }
+        };
+        String buttonText = startButton.getText().toString();
+        if (buttonText == "START") {
+            timer = new Timer();
+            startButton.setText("PAUSE");
+            timer.scheduleAtFixedRate(timerTask, 0, 1); // every millisecond
         } else {
             startButton.setText("START");
-            timer.cancel();
-            timer.purge();
+//            timer.cancel();
+            onStopTimer();
         }
+    }
+    public void onStopTimer(){
+        timer.cancel();
     }
 
     public void onResetButton(View view){
+        timer.cancel();
         currentTime = 0;
     }
 }

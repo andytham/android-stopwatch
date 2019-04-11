133a82d18ba09e2c1093bc64285248024d237982
andy <thamandyy@gmail.com>
2019-04-10 21:09:24 -0400

    Split time format function

--- src/main/java/com/example/stopwatch/MainActivity.java
+++ src/main/java/com/example/stopwatch/MainActivity.java
@@ -9,14 +9,14 @@ import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
     private int currentTime = 0;
-    private int milliseconds = 0;
-    private int seconds = 0;
-    private int minutes = 0;
-    private int hours = 0;
-    private String stringMilliseconds = "000";
-    private String stringSeconds = "00";
-    private String stringMinutes = "00";
-    private String stringHours = "00";
+//    private int milliseconds = 0;
+//    private int seconds = 0;
+//    private int minutes = 0;
+//    private int hours = 0;
+//    private String stringMilliseconds = "000";
+//    private String stringSeconds = "00";
+//    private String stringMinutes = "00";
+//    private String stringHours = "00";
     private String timeString;
     private TextView display;
     private TextView startButton;
@@ -41,33 +41,7 @@ public class MainActivity extends AppCompatActivity {
                 runOnUiThread(new Runnable() {
                     public void run() {
                         currentTime += 1;
-                        milliseconds = currentTime % 1000;
-                        seconds = (currentTime / 1000) % 60;
-                        minutes = currentTime / 1000 / 60;
-                        hours = currentTime / 1000 / 360;
-
-                        stringMilliseconds = Integer.toString(milliseconds);
-                        if (milliseconds < 100) {
-                            if (milliseconds < 10) {
-                                stringMilliseconds = "0" + stringMilliseconds;
-                            }
-                            stringMilliseconds = "0" + stringMilliseconds;
-                        }
-                        stringSeconds = Integer.toString(seconds);
-                        if (seconds < 10) {
-                            stringSeconds = "0" + Integer.toString(seconds);
-                        }
-                        stringMinutes = Integer.toString(minutes);
-                        if (minutes < 10) {
-                            stringMinutes = "0" + stringMinutes;
-                        }
-                        stringHours = Integer.toString(hours);
-                        if (hours < 10) {
-                            stringHours = "0" + stringHours;
-                        }
-
-
-                        timeString = stringHours + ":" + stringMinutes + ":" + stringSeconds + ":" + stringMilliseconds;
+                        timeString = formatTime(currentTime);
                         display.setText(timeString);
                         if (isStopped){
                             timer.cancel();
@@ -91,7 +65,34 @@ public class MainActivity extends AppCompatActivity {
     public void onStopTimer(){
         timer.cancel();
     }
+    public String formatTime(int currentTime){
+        int milliseconds = currentTime % 1000;
+        int seconds = (currentTime / 1000) % 60;
+        int minutes = currentTime / 1000 / 60;
+        int hours = currentTime / 1000 / 360;
 
+        String stringMilliseconds = Integer.toString(milliseconds);
+        if (milliseconds < 100) {
+            if (milliseconds < 10) {
+                stringMilliseconds = "0" + stringMilliseconds;
+            }
+            stringMilliseconds = "0" + stringMilliseconds;
+        }
+        String stringSeconds = Integer.toString(seconds);
+        if (seconds < 10) {
+            stringSeconds = "0" + Integer.toString(seconds);
+        }
+        String stringMinutes = Integer.toString(minutes);
+        if (minutes < 10) {
+            stringMinutes = "0" + stringMinutes;
+        }
+        String stringHours = Integer.toString(hours);
+        if (hours < 10) {
+            stringHours = "0" + stringHours;
+        }
+
+        return stringHours + ":" + stringMinutes + ":" + stringSeconds + ":" + stringMilliseconds;
+    }
     public void onResetButton(View view){
 //        timer.cancel();
         display.setText("00:00:00:000");
